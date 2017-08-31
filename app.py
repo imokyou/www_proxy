8708d80649693e7b69ba65d1e5925d9592ad4670
lupt <lupt@luptdeMacBook-Pro.local>
2017-08-31 08:49:22 +0800

    V1

---
+++ app.py
@@ -0,0 +1,47 @@
+# coding=utf8
+from flask import Flask, request, render_template
+from settings import *
+from db import RedisClient
+
+_redisdb = RedisClient()
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    proxies_source = _redisdb.get_proxies_cache()
+    if not proxies_source:
+        proxies_s = _redisdb.get_proxies_source(20)
+        proxies_ava = _redisdb.get_proxies(10)
+        proxies_source = proxies_s + proxies_ava
+        _redisdb.set_proxies_cache(proxies_source)
+
+    proxies = []
+    for p in proxies_source:
+        proto = p.split('://')[0]
+        ip, port = p.split('://')[1].split(':')
+        proxies.append({
+            'ip': ip,
+            'port': port,
+            'proto': proto.upper()
+        })
+    return render_template('index.html', proxies=proxies)
+
+@app.route("/proxy/", methods=["GET"])
+def get_proxy():
+    key = request.args.get('key', '')
+    if key != SIGNKEY:
+        return ''
+    proxy = _redisdb.rand_proxy()
+    return proxy
+
+@app.route("/proxy/count/", methods=["GET"])
+def count_proxy():
+    proxy_len = _redisdb.proxy_len
+    return proxy_len
+
+
+def main():
+    app.run()
+
+if __name__ == '__main__':
+    main()
---
+++ db.py
@@ -0,0 +1,91 @@
+# coding=utf8
+import traceback
+import urllib
+import json
+import redis
+from settings import *
+
+
+class RedisClient(object):
+
+    def __init__(self):
+        try:
+            if REDIS['password']:
+                redis_pool = redis.ConnectionPool(host=REDIS['host'], port=REDIS['port'], password=REDIS['password'], db=REDIS['db'])
+            else:
+                redis_pool = redis.ConnectionPool(host=REDIS['host'], port=REDIS['port'], db=REDIS['db'])
+            self._db = redis.Redis(connection_pool=redis_pool)
+        except:
+            logging.info('connect redis error')
+        self._proxy = 'proxy'
+        self._proxy_source = 'proxy:source'
+        self._proxy_cache = 'proxy:cache'
+
+    def add_proxy(self, proxy):
+        if not proxy:
+            return None
+        proxies = self.get_allproxy()
+        if proxy in proxies:
+            return None
+        self._db.lpush(self._proxy, proxy)
+    
+    def add_proxies(self, proxies):
+        if not proxies:
+            return None
+        for u in proxies:
+            self.add_proxies(proxies)
+
+    def add_proxy_source(self, proxy):
+        if not proxy:
+            return None
+        self._db.sadd(self._proxy_source, proxy)
+
+    def add_proxies_source(self, proxies):
+        if not proxies:
+            return None
+        for u in proxies:
+            self.add_proxy_source(u)
+
+    def rand_proxy(self):
+        try:
+            proxy = self._db.rpop(self._proxy)
+            self.add_proxy(proxy)
+            return proxy
+        except:
+            pass
+        return None
+
+    def remove_proxy(self, proxy):
+        try:
+            return self._db.lrem(self._proxy, proxy)
+        except:
+            traceback.print_exc()
+
+    def get_proxies(self, count=1):
+        proxies = self._db.lrange(self._proxy, 0, count)
+        return proxies
+        
+    def get_allproxy(self):
+        proxies = self._db.lrange(self._proxy, 0, -1)
+        return proxies
+
+    def get_proxies_source(self, count=1):
+        proxies = self._db.srandmember(self._proxy_source, count)
+        return proxies
+
+    def get_proxies_cache(self):
+        proxies = self._db.get(self._proxy_cache)
+        return json.loads(proxies)
+
+    def set_proxies_cache(self, proxies):
+        proxies = json.dumps(proxies)
+        self._db.set(self._proxy_cache, proxies)
+        self._db.expire(self._proxy_cache, 60*15)
+
+    @property
+    def proxy_len(self):
+        return self._db.llen(self._proxy)
+
+
+if __name__ == '__main__':
+    pass
